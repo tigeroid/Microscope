d7c05089a6f675672362db01e85f4afd80472956
tigeroid <tanohenry@gmail.com>
2015-01-08 17:54:20 +0800

    1) Using ian:accounts-ui-bootstrap-3 instead of default accounts-ui 2) Added notification features

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,17 @@
-Meteor.publish('posts', function() {
+Meteor.publish('posts', function() 
+{
 	return Posts.find();
 });
 
 Meteor.publish('comments',
+	function(postId) {
+		check(postId, String);
+		return Comments.find({postId: postId});
+	}
+);
+
+Meteor.publish('notifications', 
 	function() {
-		return Comments.find();
+		return Notifications.find();
 	}
 );
