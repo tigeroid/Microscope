898e1842155ed9d5678490480bcfb14bee2cf96b
tigeroid <tanohenry@gmail.com>
2015-01-20 17:58:33 +0800

    added post reordering animation

--- server/publications.js
+++ server/publications.js
@@ -24,3 +24,13 @@ Meteor.publish('notifications',
 		return Notifications.find({userId: this.userId, read: false});
 	}
 );
+
+Meteor.publish('newPosts', 
+	function(limit) {
+		return Posts.find({}, {sort: {submitted: -1}, limit: limit});
+	});
+
+Meteor.publish('bestPosts', 
+	function(limit) {
+		return Posts.find({}, {sort: {votes: -1, submitted: -1}, limit: limit});
+	});
